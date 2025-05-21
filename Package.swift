67f8d9c98c6f20ce78a995da2d0a19390ed8d1e5
iOSDeveloperGuy <101750471+iOSDeveloperGuy@users.noreply.github.com>
2025-05-21 14:25:17 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/GEOSwift/GEOSwift.git", from: "7.0.0"),
-        .package(url: "https://github.com/GEOSwift/geos.git", from: "5.0.0")
+        .package(url: "https://github.com/GEOSwift/geos.git", from: "9.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
